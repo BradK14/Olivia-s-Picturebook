132bb7e0edd4352d3fcb25d8816c8282617b4bf2
BradK14 <brad14@csu.fullerton.edu>
2026-06-08 10:59:34 -0700

    Play now cycles randomly through images

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 const IMGWIDTH = 600;
 
 const startButton = document.getElementById("startButton");
+let image;
+let inputButton;
+
+// Replace this with an array of classes called playImages
+const playImages = ['images/PlayImages/Ladybug.png', 'images/PlayImages/Elephant.png'];
+
+function NextImage(){
+    image.remove();
+
+    image = document.createElement('img');
+    image.src = playImages[Math.floor(Math.random() * playImages.length)]
+    image.alt = 'Image';
+    image.width = IMGWIDTH;
+    document.getElementsByClassName("ImageLocation")[0].appendChild(image);
+}
 
 function StartGame(){
-    const img = document.createElement('img');
-    img.src = 'images/Ladybug.png';
-    img.alt = 'Ladybug';
-    img.width = IMGWIDTH;
-    document.getElementsByClassName("ImageLocation")[0].appendChild(img);
+    image = document.createElement('img');
+    image.src = 'images/PlayImages/Ladybug.png';
+    image.alt = 'Ladybug';
+    image.width = IMGWIDTH;
+    document.getElementsByClassName("ImageLocation")[0].appendChild(image);
+
+    startButton.remove();
+
+    inputButton = document.createElement('button');
+    inputButton = document.getElementsByClassName("InputLocation")[0].appendChild(inputButton);
+    inputButton.textContent = 'Next';
+    inputButton.addEventListener("click", NextImage);
 }
 
 startButton.addEventListener("click", StartGame);
