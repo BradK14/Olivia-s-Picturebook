b69d19c0681dffa8709de98dac984060473d537f
BradK14 <brad14@csu.fullerton.edu>
2026-06-07 20:38:46 -0700

    Added first js code, the start game button

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const IMGWIDTH = 600;
 
+const startButton = document.getElementById("startButton");
+
+function StartGame(){
+    const img = document.createElement('img');
+    img.src = 'images/Ladybug.png';
+    img.alt = 'Ladybug';
+    img.width = IMGWIDTH;
+    document.getElementsByClassName("ImageLocation")[0].appendChild(img);
+}
+
+startButton.addEventListener("click", StartGame);
