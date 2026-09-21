8d33e34ff731c2e2fef08a3c3b7e64c09a787e4b
BradK14 <brad14@csu.fullerton.edu>
2026-09-20 21:14:31 -0700

    playImages now populates with default images for first time visitors

--- public/scripts/playImages.js
+++ public/scripts/playImages.js
@@ -6,7 +6,11 @@ async function getPlayImages(){
     // Use play images from local storage
     const piJSON = localStorage.getItem('playImages');
     if (!piJSON){
-        playImages = [];
+        // If this is the first time opening Olivia's Picturebook, populate playImages local variable with default images from the server
+        const response = await fetch("/Olivia's_Picturebook/save");
+        const { playImages: pi } = await response.json();
+        playImages = pi;
+        localStorage.setItem('playImages', JSON.stringify(playImages));
     }
     else{
         playImages = JSON.parse(piJSON);
